9fe3873c4dc6a43b9f3c39a45ba046b092ff8863
MansiJoshi1701 <mansijoshi.mj1798@gmail.com>
2025-07-29 17:29:21 -0700

    backend logic added

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,36 @@ function App() {
 
   const [products , setProducts] = useState([]);
   const [page , setPage] = useState(1);
+  //const [totalPages , setTotalPages] = useState(0);
 
   
   const fetchData = async () => {
 
 
-    const res = await fetch("https://dummyjson.com/products?limit=100");
+    const res = await fetch("https://dummyjson.com/products?limit=100");  //-->frontend driven approach
+    //const res = await fetch(`https://dummyjson.com/products?limit=10&skip=${page*10-10}`); //backend driven approach
+    //limit & skip = only returns 'limit' no. of products at a time and omits (from the start) 'skip' no. of products
     const data = await res.json();
 
     if(data && data.products){
 
       setProducts(data.products);
+      //setTotalPages(data.total / 10);
     }
     
   };
   
-  console.log(products);
 
   useEffect(() => {
     fetchData();
+
+    //if using the backend driven approach, dependency array will have [page]
   },[])
 
 
   const selectPageHandler = (selectedPage) => {
 
-    if(selectedPage >= 1 && selectedPage <= products.length / 10){
+    if(selectedPage >= 1 && selectedPage <= products.length/10){
 
       setPage(selectedPage);
     }
@@ -41,7 +46,8 @@ function App() {
     <div>
       {products.length > 0 && (
         <div className="products">
-          {products.slice(page * 10 - 10, page * 10).map((prod) => {
+          {products.slice(page*10-10 , page*10).map((prod) => {
+          {/* {products.map((prod) => { */}
             return(
               <span className="products__single" key={prod.id}>
                 <img src={prod.images} alt={prod.title} />
@@ -65,7 +71,9 @@ function App() {
           //These "empty slots" are not actual undefined values, and some array methods like map might skip them.
           //spread syntax [...] converts those empty slots into undefined values e.g. [undefined, undefined, undefined...],
           //which ensures that methods like map will iterate over them.
-           [...Array(products.length / 10)].map((_,i) =>{
+
+          [...Array(products.length/10)].map((_,i) =>{  
+          //  [...Array(totalPages)].map((_,i) =>{  -->backend driven approach
             return(
               <span
                 className={page === i+1 ? "pagination__selected" : ""}
@@ -78,7 +86,8 @@ function App() {
           }
           <span 
             onClick={() => selectPageHandler(page+1)}
-            className={page < products.length/10 ? "" : "pagination__disable"}
+            className={page < products.length/10 ? "" : "pagination__disable"}  
+            //</div>className={page < totalPages ? "" : "pagination__disable"}  -->backend driven approach
           >
             ⏭️
           </span>
